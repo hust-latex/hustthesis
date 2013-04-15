53d1c5966f6e84f26849c71b178915356acb4b42
Xu Cheng <xucheng@me.com>
2013-04-15 16:55:21 +0800

    More stable unix path handle

--- install/unix.sh
+++ install/unix.sh
@@ -9,29 +9,29 @@ TEXMFLOCAL=`kpsewhich --var-value=TEXMFLOCAL | awk '{print $1}' `
 
 install(){
 echo "Install hustthesis.cls template into local."
-mkdir -p ${TEXMFLOCAL}/tex/generic/hustthesis/
-cp -f ./hustthesis/hust-thesis-var.tex ${TEXMFLOCAL}/tex/generic/hustthesis/
-cp -f ./hustthesis/hust-title.eps ${TEXMFLOCAL}/tex/generic/hustthesis/
-cp -f ./hustthesis/hust-title.pdf ${TEXMFLOCAL}/tex/generic/hustthesis/
-cp -f ./hustthesis/hustthesis.cls ${TEXMFLOCAL}/tex/generic/hustthesis/
-mkdir -p ${TEXMFLOCAL}/bibtex/bst/hustthesis/
-cp -f ./hustthesis/hustthesis.bst ${TEXMFLOCAL}/bibtex/bst/hustthesis/
-mkdir -p ${TEXMFLOCAL}/doc/generic/hustthesis/
-cp -f ./doc/hustthesis-doc.pdf ${TEXMFLOCAL}/doc/generic/hustthesis/
-cp -f ./doc/hustthesis-doc.tex ${TEXMFLOCAL}/doc/generic/hustthesis/
-mkdir -p ${TEXMFLOCAL}/doc/generic/hustthesis/example/
-cp -f ./example/hustthesis-example.pdf ${TEXMFLOCAL}/doc/generic/hustthesis/example/
-cp -f ./example/hustthesis-example.tex ${TEXMFLOCAL}/doc/generic/hustthesis/example/
-cp -f ./example/fig-example.pdf ${TEXMFLOCAL}/doc/generic/hustthesis/example/
-cp -f ./example/ref-example.bib ${TEXMFLOCAL}/doc/generic/hustthesis/example/
+mkdir -p "${TEXMFLOCAL}/tex/generic/hustthesis/"
+cp -f ./hustthesis/hust-thesis-var.tex "${TEXMFLOCAL}/tex/generic/hustthesis/"
+cp -f ./hustthesis/hust-title.eps "${TEXMFLOCAL}/tex/generic/hustthesis/"
+cp -f ./hustthesis/hust-title.pdf "${TEXMFLOCAL}/tex/generic/hustthesis/"
+cp -f ./hustthesis/hustthesis.cls "${TEXMFLOCAL}/tex/generic/hustthesis/"
+mkdir -p "${TEXMFLOCAL}/bibtex/bst/hustthesis/"
+cp -f ./hustthesis/hustthesis.bst "${TEXMFLOCAL}/bibtex/bst/hustthesis/"
+mkdir -p "${TEXMFLOCAL}/doc/generic/hustthesis/"
+cp -f ./doc/hustthesis-doc.pdf "${TEXMFLOCAL}/doc/generic/hustthesis/"
+cp -f ./doc/hustthesis-doc.tex "${TEXMFLOCAL}/doc/generic/hustthesis/"
+mkdir -p "${TEXMFLOCAL}/doc/generic/hustthesis/example/"
+cp -f ./example/hustthesis-example.pdf "${TEXMFLOCAL}/doc/generic/hustthesis/example/"
+cp -f ./example/hustthesis-example.tex "${TEXMFLOCAL}/doc/generic/hustthesis/example/"
+cp -f ./example/fig-example.pdf "${TEXMFLOCAL}/doc/generic/hustthesis/example/"
+cp -f ./example/ref-example.bib "${TEXMFLOCAL}/doc/generic/hustthesis/example/"
 hash
 }
 
 uninstall(){
 echo "Uninstall hustthesis.cls template."
-rm -rf ${TEXMFLOCAL}/tex/generic/hustthesis/
-rm -rf ${TEXMFLOCAL}/bibtex/bst/hustthesis/
-rm -rf ${TEXMFLOCAL}/doc/generic/hustthesis/
+rm -rf "${TEXMFLOCAL}/tex/generic/hustthesis/"
+rm -rf "${TEXMFLOCAL}/bibtex/bst/hustthesis/"
+rm -rf "${TEXMFLOCAL}/doc/generic/hustthesis/"
 hash
 }
 
