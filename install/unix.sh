d042b08753715ef98cc4b1a1a8f7b5d42615ed55
Xu Cheng <xucheng@me.com>
2014-03-20 17:03:36 +0800

    script

--- install/unix.sh
+++ install/unix.sh
@@ -1,6 +1,22 @@
-#!/usr/bin/bash
+#!/usr/bin/env bash
 # LaTeX package install script
 # Author: Xu Cheng
+
+command_exists () {
+    type "$1" &> /dev/null ;
+}
+
+if ! command_exists realpath ; then
+    if command_exists grealpath ; then
+        realpath() {
+            grealpath "$1"
+        }
+    else
+        realpath() {
+            [[ $1 = /* ]] && echo "$1" || echo "$PWD/${1#./}"
+        }
+    fi
+fi
 
 SCRIPT_PATH=$(realpath $(cd $(dirname $0);pwd))
 cd $SCRIPT_PATH/..
